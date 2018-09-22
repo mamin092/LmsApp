691af39db4a51c3444967b386d2cd294cac04a55
Mohammad Nurul Amin <mnurulamin092@outlook.com>
2018-09-22 07:07:27 +0600

    lms service

---
+++ Service/CourseService.cs
@@ -0,0 +1,20 @@
+﻿using Model;
+using Model.Repo;
+using RequestModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ViewModel;
+
+namespace Service
+{
+    public class CourseService : GenericService<Course, CourseRequestModel, CourseViewModel>
+    {
+        public CourseService(IGenericRepository<Course> repository) : base(repository)
+        {
+
+        }
+
+
+    }
+}
---
+++ Service/TeacherService.cs
@@ -0,0 +1,17 @@
+﻿using Model;
+using RequestModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ViewModel;
+using Model.Repo;
+
+namespace Service
+{
+    public class TeacherService : GenericService<Teacher, TeacherRequestModel, TeacherViewModel>
+    {
+        public TeacherService(IGenericRepository<Teacher> repository) : base(repository)
+        {
+        }
+    }
+}
