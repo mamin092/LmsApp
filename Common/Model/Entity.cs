da8045e7a43110862ae261f82f3fab195d5e847c
Mohammad Nurul Amin <mnurulamin092@outlook.com>
2018-09-22 07:03:57 +0600

    added common

---
+++ Common/Model/Entity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Model
+{
+    [Serializable]
+    public class Entity
+    {
+        public string Id { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public DateTime Modified { get; set; }
+
+        public string ModifiedBy { get; set; }
+
+        public bool IsActive { get; set; } 
+
+        public bool IsDeleted { get; set; } 
+
+    }
+}
