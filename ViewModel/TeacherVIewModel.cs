19bda9ede14ab232de4f3bd7cb1ef29f062ee1ca
Mohammad Nurul Amin <mnurulamin092@outlook.com>
2018-09-22 07:09:22 +0600

    lms view model

---
+++ ViewModel/TeacherVIewModel.cs
@@ -0,0 +1,25 @@
+﻿using Common.ViewModel;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Common.Model;
+
+namespace ViewModel
+{
+    public class TeacherViewModel : BaseViewModel<Teacher>
+    {
+        public TeacherViewModel(Teacher teacher) : base(teacher)
+        {
+            Name = teacher.Name;
+            Phone = teacher.Phone;
+            Course = teacher.Course;
+            TotalCredit = teacher.TotalCredit;
+        }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Course { get; set; }
+        public double TotalCredit { get; set; }
+
+    }
+}
